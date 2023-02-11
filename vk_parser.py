ca47dbaa4f13e9827db7cc5f07a1cbaefbffe17e
fedosique <f-alexandrov@mail.ru>
2023-02-11 12:49:37 +0300

    IOerror exception, file counter fix

--- vk_parser.py
+++ vk_parser.py
@@ -2,13 +2,17 @@ import bs4
 from bs4 import BeautifulSoup
 import re, requests
 import pandas as pd
+from pathlib import Path
 
 
-def loading(load_from, img_num):                                 ## выгрузка фотографий
-        load = requests.get(load_from.text)                      ## в будущем можно добавить multithreading
-        out = open(".\img{}.jpg".format(img_num), 'wb')
-        out.write(load.content)
-        out.close()
+def loading(load_from, img_num):                                                 ## выгрузка фотографий
+        try:
+            load = requests.get(load_from.text, timeout=15)                      ## в будущем можно добавить multithreading
+            out = open(".\img{}.jpg".format(img_num), 'wb')
+            out.write(load.content)
+            out.close()
+        except IOError:
+            pass
 
 folder = "758476599"
 link = []
@@ -16,7 +20,16 @@ link = []
 messages = 0
 epoch = 1
 
-while messages <= 22050: 
+
+def file_counter():
+        folder_name = Path(folder)
+        if folder_name.is_dir():
+            return len([1 for file in folder_name.iterdir()])
+
+
+files = file_counter()
+
+while messages < files*50: 
     source = "./{}/messages{}.html".format(folder, messages)
     file = open(source, "r")
     page_response = bs4.BeautifulSoup(file.read(), "html.parser")
@@ -30,6 +43,7 @@ while messages <= 22050:
         else:
             loading(trs, epoch)
             link.append(trs.text)
+    file.close()
     messages += 50
     epoch += 1
 
